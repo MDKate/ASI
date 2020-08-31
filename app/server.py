fa1962f802fa3175f44bdc55e1f95515b7f1e67d
Ekaterina <61936207+MDKate@users.noreply.github.com>
2020-08-31 19:09:59 +0700

    Update server.py
    1

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/zi2u779uz6qrygd/covid-19CT.pkl?dl=1'
+export_file_name = 'covid-19CT.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['COVID-19', 'Non COVID-19']
 path = Path(__file__).parent
 
 app = Starlette()
